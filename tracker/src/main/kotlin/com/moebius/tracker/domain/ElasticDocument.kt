5de96ec8b0d7306e5bbebe3b0ec0601bbed6002b
knunu <burette@hanyang.ac.kr>
2020-07-25 00:27:24 +0900

    [MBS-0] Remove all dependencies except app & backend domain.

--- tracker/src/main/kotlin/com/moebius/tracker/domain/ElasticDocument.kt
+++
@@ -1,8 +0,0 @@
-package com.moebius.tracker.domain
-
-import com.fasterxml.jackson.annotation.JsonIgnore
-
-interface ElasticDocument {
-    @JsonIgnore(true)
-    fun getDocumentId(): String
-}
--- tracker/src/main/kotlin/com/moebius/tracker/dto/upbit/UpbitMarketDto.kt
+++
@@ -1,8 +0,0 @@
-package com.moebius.tracker.dto.upbit
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-data class UpbitMarketDto(
-        val market: String
-)
