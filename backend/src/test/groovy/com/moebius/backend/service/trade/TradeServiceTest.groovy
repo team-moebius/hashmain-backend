2284e036693ad394cb608a4b273f9387a3f6f83e
knunu <burette@hanyang.ac.kr>
2020-08-01 11:37:36 +0900

    [HMB-1] Implement trade service test.

--- backend/src/test/groovy/com/moebius/backend/service/trade/TradeServiceTest.groovy
+++ backend/src/test/groovy/com/moebius/backend/service/trade/TradeServiceTest.groovy
@@ -1,8 +1,24 @@
 package com.moebius.backend.service.trade
 
+import com.moebius.backend.service.kafka.consumer.TradeKafkaConsumer
+import org.springframework.boot.context.event.ApplicationReadyEvent
 import spock.lang.Specification
+import spock.lang.Subject
 
 class TradeServiceTest extends Specification {
-	def "OnApplicationEvent"() {
+	def tradeKafkaConsumer = Mock(TradeKafkaConsumer)
+
+	@Subject
+	def tradeService = new TradeService(tradeKafkaConsumer)
+
+	def "Should start to consume on application ready event"() {
+		given:
+		def event = Stub(ApplicationReadyEvent)
+
+		when:
+		tradeService.onApplicationEvent(event)
+
+		then:
+		1 * tradeKafkaConsumer.consumeMessages()
 	}
 }
