fc33a4598e3027dcb3c5efa9bdf2eeffaa3c32c0
knunu <burette@hanyang.ac.kr>
2020-08-02 23:30:35 +0900

    [HMB-0] update gradle of swagger to 3.0.0

--- backend/src/main/java/com/moebius/backend/configuration/SwaggerConfiguration.java
+++ backend/src/main/java/com/moebius/backend/configuration/SwaggerConfiguration.java
@@ -14,7 +14,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
 import java.security.Principal;
 
 @Configuration
-@EnableSwagger2WebFlux
 public class SwaggerConfiguration {
 
 	@Bean
