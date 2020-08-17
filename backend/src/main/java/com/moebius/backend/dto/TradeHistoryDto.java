0d7c2f555f43fda54a0a06cabefcd9f054c17219
knunu <burette@hanyang.ac.kr>
2020-08-17 23:57:51 +0900

    [HMB-2] Implement trade history dto for data api.

--- backend/src/main/java/com/moebius/backend/dto/TradeHistoryDto.java
+++ backend/src/main/java/com/moebius/backend/dto/TradeHistoryDto.java
@@ -1,11 +1,26 @@
 package com.moebius.backend.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.moebius.backend.domain.commons.Change;
+import com.moebius.backend.domain.commons.Exchange;
+import com.moebius.backend.domain.commons.TradeType;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Builder
 @ToString
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class TradeHistoryDto {
+	private Exchange exchange;
+	private String symbol;
+	private TradeType tradeType;
+	private Change change;
+	private double price;
+	private double volume;
+	private double prevClosingPrice;
+	private LocalDateTime createdAt;
 }
