b5ecc9624d57472aa9ec3659e88dd64e1d3058ee
knunu <burette@hanyang.ac.kr>
2020-08-25 23:28:09 +0900

    [HMB-8] Implement prototype of slack sender.

--- backend/src/main/java/com/moebius/backend/service/slack/SlackSender.java
+++ backend/src/main/java/com/moebius/backend/service/slack/SlackSender.java
@@ -1,4 +1,25 @@
 package com.moebius.backend.service.slack;
 
-public class SlackSender {
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Slf4j
+@RequiredArgsConstructor
+public abstract class SlackSender<T> {
+	private final WebClient webClient;
+
+	protected abstract String getWebHookUrl();
+
+	public void sendMessage(T message) {
+		log.info("[Slack] Start to send slack message [{}]", message);
+
+		webClient.post()
+			.uri(getWebHookUrl())
+			.contentType(MediaType.APPLICATION_JSON)
+			.bodyValue(message)
+			.exchange()
+			.subscribe();
+	}
 }
--- backend/src/main/java/com/moebius/backend/service/slack/TradeSlackSender.java
+++ backend/src/main/java/com/moebius/backend/service/slack/TradeSlackSender.java
@@ -1,4 +1,21 @@
 package com.moebius.backend.service.slack;
 
-public class TradeSlackSender {
+import com.moebius.backend.dto.SlackMessageDto;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Component
+public class TradeSlackSender extends SlackSender<SlackMessageDto> {
+	@Value("${slack.web-hook-url.trade}")
+	private String webHookUrl;
+
+	public TradeSlackSender(WebClient webClient) {
+		super(webClient);
+	}
+
+	@Override
+	protected String getWebHookUrl() {
+		return webHookUrl;
+	}
 }
