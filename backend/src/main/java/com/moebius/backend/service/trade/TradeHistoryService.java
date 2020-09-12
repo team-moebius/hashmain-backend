9072de9407217633a2e0b4606e9a169d3fe68d5b
knunu <burette@hanyang.ac.kr>
2020-09-12 23:53:14 +0900

    [HMB-2] Update trade history service.

--- backend/src/main/java/com/moebius/backend/service/trade/TradeHistoryService.java
+++ backend/src/main/java/com/moebius/backend/service/trade/TradeHistoryService.java
@@ -22,6 +22,7 @@ public class TradeHistoryService {
 	private static final String COLON = ":";
 	private static final String SLASH = "/";
 	private static final String QUESTION = "?";
+	private static final String COUNT_CONDITION = "count=";
 	private static final String TIME_CONDITION = "minutesAgo=";
 
 	@Value("${moebius.data.host}")
@@ -35,11 +36,11 @@ public class TradeHistoryService {
 
 	private final WebClient webClient;
 
-	public Flux<TradeHistoryDto> getTradeHistories(Exchange exchange, String symbol, int minutesAgo) {
+	public Flux<TradeHistoryDto> getTradeHistories(Exchange exchange, String symbol, int count) {
 		String pathParameters = SLASH + exchange + SLASH + symbol;
 
 		return webClient.get()
-			.uri(dataApiHost + COLON + dataApiPort + tradeHistoriesUrl + pathParameters + QUESTION + TIME_CONDITION + minutesAgo)
+			.uri(dataApiHost + COLON + dataApiPort + tradeHistoriesUrl + pathParameters + QUESTION + COUNT_CONDITION + count)
 			.retrieve()
 			.bodyToFlux(TradeHistoryDto.class)
 			.doOnError(exception -> log.warn("[Trade] Failed to get aggregated trade history.", exception))
@@ -55,7 +56,6 @@ public class TradeHistoryService {
 			.retrieve()
 			.bodyToMono(AggregatedTradeHistoryDto.class)
 			.doOnError(exception -> log.warn("[Trade] Failed to get aggregated trade history.", exception))
-			.onErrorReturn(WebClientResponseException.class, AggregatedTradeHistoryDto.builder().build())
-			.cache(Duration.ofMinutes(10));
+			.onErrorReturn(WebClientResponseException.class, AggregatedTradeHistoryDto.builder().build());
 	}
 }
