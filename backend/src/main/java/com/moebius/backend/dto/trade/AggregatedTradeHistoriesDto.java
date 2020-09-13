8bac4a42276026ae010c8bf72bdfc99528d3d2ba
knunu <burette@hanyang.ac.kr>
2020-09-13 23:58:45 +0900

    [HMB-12] Implementing CODT phase 2 ...

---
+++ backend/src/main/java/com/moebius/backend/dto/trade/AggregatedTradeHistoriesDto.java
@@ -0,0 +1,27 @@
+package com.moebius.backend.dto.trade;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+import com.moebius.backend.domain.commons.Exchange;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@Builder(builderClassName = "AggregatedTradeHistoriesDtoBuilder")
+@ToString
+@JsonDeserialize(builder = AggregatedTradeHistoriesDto.AggregatedTradeHistoriesDtoBuilder.class)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class AggregatedTradeHistoriesDto {
+	private Exchange exchange;
+	private String symbol;
+	private long interval;
+	private List<AggregatedTradeHistoryDto> aggregatedTradeHistories;
+
+	@JsonPOJOBuilder(withPrefix = "")
+	public static class AggregatedTradeHistoriesDtoBuilder {
+	}
+}
