c3b847332bb9199a2edc55245e84fae7e6b80761
knunu <burette@hanyang.ac.kr>
2020-09-30 14:38:51 +0900

    [HMB-12] change the condition of valid trade.

--- backend/src/main/java/com/moebius/backend/service/trade/TradeService.java
+++ backend/src/main/java/com/moebius/backend/service/trade/TradeService.java
@@ -27,8 +27,8 @@ public class TradeService {
 	private static final double TRADE_PRICE_THRESHOLD = 10000D;
 	private static final double TRADE_HISTORY_PRICE_THRESHOLD = 5000000D;
 	private static final double HISTORY_VOLUME_MULTIPLIER_THRESHOLD = 5D;
-	private static final double VALID_RISING_PRICE_CHANGE_THRESHOLD = 1.03D;
-	private static final double VALID_FALLING_PRICE_CHANGE_THRESHOLD = 0.97D;
+	private static final double VALID_RISING_PRICE_CHANGE_THRESHOLD = 1.02D;
+	private static final double VALID_FALLING_PRICE_CHANGE_THRESHOLD = 0.98D;
 
 	public void identifyValidTrade(TradeDto tradeDto) {
 		if (isValidTrade(tradeDto)) {
@@ -55,7 +55,7 @@ public class TradeService {
 	 *
 	 * 2. Price
 	 * 2-1. Heavy total transaction price : valid accumulated price is over 5M KRW
-	 * 2-2. Heavy total transaction price change : the last history has greater than equal to +-3% price change than previous earliest history's price.
+	 * 2-2. Heavy total transaction price change : the last history has greater than equal to +-2% price change than previous earliest history's price.
 	 *
 	 * @param historiesDto
 	 * @return
@@ -73,7 +73,7 @@ public class TradeService {
 			.orElse(0D);
 		double earliestTradePrice = histories.get(0).getTotalTransactionPrice() / histories.get(0).getTotalTransactionVolume();
 
-		if (isValidVolume(latestHistory, previousAverageVolume) ||
+		if (isValidVolume(latestHistory, previousAverageVolume) &&
 			isValidPrice(latestHistory, earliestTradePrice)) {
 			log.info("[Trade] [{}/{}] The valid trade histories exist. [TTV: {}, PAV: {}, PAP: {}, PVP: {}]",
 				historiesDto.getExchange(), historiesDto.getSymbol(), latestHistory.getTotalTransactionVolume(), previousAverageVolume,
