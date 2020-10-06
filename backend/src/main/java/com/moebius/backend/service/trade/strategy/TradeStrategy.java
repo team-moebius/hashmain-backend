1f7b44f04d4c6a0dbb0173ec2ded241ebc186759
knunu <burette@hanyang.ac.kr>
2020-10-06 23:50:38 +0900

    [HMB-12] Implement Trade Strategy interface.

--- backend/src/main/java/com/moebius/backend/service/trade/strategy/TradeStrategy.java
+++ backend/src/main/java/com/moebius/backend/service/trade/strategy/TradeStrategy.java
@@ -1,4 +1,9 @@
 package com.moebius.backend.service.trade.strategy;
 
+import com.moebius.backend.dto.trade.AggregatedTradeHistoriesDto;
+import com.moebius.backend.dto.trade.TradeDto;
+
+@FunctionalInterface
 public interface TradeStrategy {
+	boolean isValid(TradeDto tradeDto, AggregatedTradeHistoriesDto historiesDto);
 }
