65d156e63fc64f674725190717242e3dc6040ce9
knunu <burette@hanyang.ac.kr>
2020-10-20 23:58:49 +0900

    [HMB-19] Add heavy valid trade strategy prototype.

--- backend/src/main/java/com/moebius/backend/service/trade/strategy/HeavyValidTradeStrategy.java
+++ backend/src/main/java/com/moebius/backend/service/trade/strategy/HeavyValidTradeStrategy.java
@@ -2,16 +2,21 @@ package com.moebius.backend.service.trade.strategy;
 
 import com.moebius.backend.dto.trade.AggregatedTradeHistoriesDto;
 import com.moebius.backend.dto.trade.TradeDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 
+
+@Slf4j
+@Component
 public class HeavyValidTradeStrategy implements TradeStrategy {
 	@Override
 	public int getTimeInterval() {
-		return 0;
+		return 1;
 	}
 
 	@Override
 	public int getTimeRange() {
-		return 0;
+		return 3;
 	}
 
 	@Override
